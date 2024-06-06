cf6edca0764a722e59b9eb977ea4f8e26c9dad1f
Mayank Lohani <mayanklohani3@gmail.com>
2024-06-06 15:06:08 +0530

    rebaseing from main

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,12 @@
 import './App.css';
 import * as React from 'react';
-
 import Header from "./components/header/Header"
 import Editorpage from "./components/page/editor_pg/Editorpage"
 function App() {
   return (
     <div className="App">
-      <div className  = "title">Policy Generator</div>
-      <div className ="Base_Command">Set</div>
-      <div> 
-        <label>
-            <select>
 
-              <option value="interface">Interface</option>
-
-              <option value="protocol bgp">Protocol Bgp</option>
-
-              <option value="system">Systems</option>
-
-            </select>
-  
-      </label>
-</div>
-    </div>
+      </div> 
   );
 }
 
