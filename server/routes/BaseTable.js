e8419b3e789c8a8440f7afe7ea32821f6ead1514
Mayank Lohani <mayanklohani3@gmail.com>
2024-06-12 10:59:20 +0530

    link basecommands to systemCommands

--- server/routes/BaseTable.js
+++ server/routes/BaseTable.js
@@ -5,7 +5,6 @@ const { BaseTable }  = require("../models");
 
 
 router.get("/",async (req,res) => {
-<<<<<<< HEAD:server/routes/BaseTable.js
 	const listSetCommands = await BaseTable.findAll();
 	res.json(listSetCommands);
 });
@@ -13,13 +12,6 @@ router.post("/",async (req,res) => {
 	const basecommands = req.body;
 	await BaseTable.create(basecommands);
 	res.json(basecommands);
-=======
-	const listSetCommands = await BaseCommands.findAll();
-});
-router.post("/",async (req,res) => {
-	const basecommands = req.body;
-	await BaseCommands.create(basecommands);
->>>>>>> 5485325 (link basecommands to systemCommands):server/routes/BaseCommands.js
 });
 
 module.exports = router;
