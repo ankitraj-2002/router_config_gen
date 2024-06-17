35080747ff9fb044d52b54092e2e9c639c82eea8
Mayank Lohani <mayanklohani3@gmail.com>
2024-06-17 13:11:28 +0530

    resolved errors after merging with mayank-branch

--- client/src/components/page/homepage/editor/Editor.jsx
+++ client/src/components/page/homepage/editor/Editor.jsx
@@ -4,7 +4,6 @@ import Textbox from './textbox/Textbox';
 import Options from './options/Options';
 import Display from './display/Display';
 import CurrentCommandDisplay from './curcmddisp/CurrentCommandDisplay';
-// import './curcmddisp.css';
 import './curcmddisp/curcmddisp.css';
 
 const Editor = () => {
@@ -42,62 +41,6 @@ const Editor = () => {
       resetNextCommand.current();
     }
   };
-
-//   return (<>
-//     <div className = "left-panel">
-//       <Display CommandLine = {appendLine}  />
-//     </div>
-//     <div className = "right-panel">
-//       <CurrentCommandDisplay commandString={appendedText}/>
-//       {showTextbox ? (
-//         <>
-//           <Textbox text={inputText} onTextChange={handleTextChange} />
-//           <button onClick={handlePushText}>Push Text</button>
-//         </>
-//       ) : (
-//         <>
-//           <Options onAppendText = {handleAppendText} />
-//           <button className="button" onClick={handleClick}>
-//             Add Manually
-//           </button>
-//           <button className="button" onClick={handleAddNewline}>
-//             Next-Command
-//           </button>
-//         </>
-//       )}
-//     </div>
-//     </>
-//   );
-// };
-// return (
-//   <>
-//     <div className="left-panel">
-//       <Display CommandLine={appendLine} />
-//     </div>
-//     <div className="right-panel">
-//       <CurrentCommandDisplay commandString={appendedText} />
-//       {showTextbox ? (
-//         <>
-//           <Textbox text={inputText} onTextChange={handleTextChange} />
-//           <button onClick={handlePushText}>Push Text</button>
-//         </>
-//       ) : (
-//         <div className="button-container">
-//           <Options onAppendText={handleAppendText} />
-//           <button className="button" onClick={handleClick}>
-//             Add Manually
-//           </button>
-//           <button className="button" onClick={handleAddNewline}>
-//             Next-Command
-//           </button>
-//         </div>
-//       )}
-//     </div>
-//   </>
-// );
-// };
-
-// export default Editor;
 return (
   <>
     <div className='left-panel'>
@@ -111,14 +54,10 @@ return (
           <button onClick={handlePushText} className='button'>Push Text</button>
         </>
       ) : (
-        <div className='options-container'>
-          <Options onAppendText={handleAppendText} />
-          <button className='button' onClick={handleClick}>
-            Add Manually
-        <>
+        <div classsName = "option-container">
           <Options onAppendText={handleAppendText} resetNextCommand={resetNextCommand} />
-          <button className="my-custom-button" onClick={handleClick}>
-            Don't see the required command... Add manually from here
+          <button className="button" onClick={handleClick}>
+            Add manually
           </button>
           <button className='button' onClick={handleAddNewline}>
             Next-Command
--- client/src/components/page/homepage/editor/options/Options.jsx
+++ client/src/components/page/homepage/editor/options/Options.jsx
@@ -1,100 +1,89 @@
-/*
+// import './options.css';
+// import axios from 'axios';
+// import {useEffect,useState } from 'react';
+
+
+// const Options = ({ onAppendText, resetNextCommand }) => {
+//   const [CommandList, setCommandList] = useState([]);
+//   const [NextCommand, setNextCommand] = useState("BaseTable");
+//   const [showTextbox, setShowTextbox] = useState(false);
+//   const [textboxValue, setTextboxValue] = useState('');
+//   const [showTextboxPlaceholder, setShowTextboxPlaceholder] = useState('');
+//   const [currentItem, setCurrentItem] = useState(null);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get(`http://localhost:3001/${NextCommand}`);
+//         setCommandList(response.data);
+//       } catch (error) {
+//         console.error("Error fetching data: ", error);
+//       }
+//     };
+//   }, [resetNextCommand]);
+
+//   const onButtonClick = (item) => {
+//     setCurrentItem(item);
+//     if (item[2]) {
+//       setShowTextboxPlaceholder(item[2]);
+//       setShowTextbox(true);
+//     } else {
+//       setShowTextbox(false);
+//       onAppendText(item[1]);
+//       if (item[3])
+//     if (currentItem) {
+//       onAppendText(currentItem[1] + " " + textboxValue);
+//       setTextboxValue('');
+//       setShowTextbox(false);
+//       if (currentItem[3]) {
+//         setNextCommand(currentItem[3]);
+//       }
+//       setCurrentItem(null);
+//     }
+//   };
+
+//   const ButtonList = ({ items }) => {
+//     return (
+//       <div className="button">
+//         {items.map((item, index) => (
+//           <button className = "button" key={index} onClick={() => onButtonClick(item)}>
+//             {item[1]}
+//           </button>
+//         ))}
+//       </div>
+//     );
+//   };
+
+//   let items = [];
+//   CommandList.forEach((ele) => {
+//     items.push([ele.id, ele.BaseCommand, ele.UI_enabled, ele.NextTable]);
+//   });
+
+// return (
+//   <div className='options'>
+//     <h1 id='heading'>Select Commands</h1>
+//     <ButtonList items={items} />
+//     {showTextbox && (
+//       <div>
+//         <input
+//           type='text'
+//           value={textboxValue}
+//           onChange={handleTextboxChange}
+//           placeholder={showTextboxPlaceholder}
+//           className='textbox'
+//         />
+//         <button onClick={handleTextboxSubmit} className='button'>Submit</button>
+//       </div>
+//     )}
+//   </div>
+// );
+// };
+
+// export default Options;
+
 import './options.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-
-const Options = ({ onAppendText, resetNextCommand }) => {
-  const [CommandList, setCommandList] = useState([]);
-  const [NextCommand, setNextCommand] = useState("BaseTable");
-  const [showTextbox, setShowTextbox] = useState(false);
-  const [textboxValue, setTextboxValue] = useState('');
-  const [showTextboxValue, setShowTextboxValue] = useState('');
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`http://localhost:3001/${NextCommand}`);
-        setCommandList(response.data);
-      } catch (error) {
-        console.error("Error fetching data: ", error);
-      }
-    };
-    fetchData();
-  }, [NextCommand]);
-
-  useEffect(() => {
-    resetNextCommand.current = () => setNextCommand("BaseTable");
-  }, [resetNextCommand]);
-
-  const onButtonClick = (item) => {
-    if(item[2] && item[3]){
-
-    }
-    else if(item[2]){
-
-    }
-    else{
-      setNextCommand(item[3]);
-      onAppendText(item[1]);
-    }
-
-    // if (item[2]) {
-    //   setShowTextboxValue(item[2]);
-    //   setShowTextbox(item[2] !== null);
-    // }
-  };
-
-  // const handleTextboxChange = (event) => {
-  //   setTextboxValue(event.target.value);
-  // };
-
-  // const handleTextboxSubmit = () => {
-  //   onAppendText(textboxValue);
-  //   setTextboxValue('');
-  //   setShowTextbox(false);
-  // };
-
-  const ButtonList = ({ items }) => (
-    <div className="button">
-      {items.map((item, index) => (
-        <button key={index} onClick={() => onButtonClick(item)}>
-          {item[1]}
-        </button>
-      ))}
-    </div>
-  );
-
-  let items = [];
-  CommandList.forEach((ele) => {
-    items.push([ele.id, ele.BaseCommand, ele.UI_enabled, ele.NextTable]);
-  });
-
-  return (
-    <div className='options'>
-      <h1>List of Buttons</h1>
-      <ButtonList items={items} />
-      {/* {showTextbox && (
-        <div>
-          <input
-            type="text"
-            value={textboxValue}
-            onChange={handleTextboxChange}
-            placeholder={showTextboxValue}
-          />
-          <button onClick={handleTextboxSubmit}>Submit</button>
-        </div>
-      )} } // take care of t his line
-    </div>
-  );
-};
-
-export default Options;
-*/
-import './options.css';
-import axios from 'axios';
-import {useEffect,useState } from 'react';
-import Display from '../../display/Display';
-
 
 const Options = ({ onAppendText, resetNextCommand }) => {
   const [CommandList, setCommandList] = useState([]);
@@ -150,62 +139,39 @@ const Options = ({ onAppendText, resetNextCommand }) => {
     }
   };
 
-  const ButtonList = ({ items }) => {
-    return (
-      <div className="button">
-        {items.map((item, index) => (
-          <button className = "button" key={index} onClick={() => onbuttonclick(item)}>
-            {item[1]}
-          </button>
-        ))}
-      </div>
-    );
-  };
+  const ButtonList = ({ items }) => (
+    <div className="button">
+      {items.map((item, index) => (
+        <button key={index} onClick={() => onButtonClick(item)}>
+          {item[1]}
+        </button>
+      ))}
+    </div>
+  );
 
   let items = [];
   CommandList.forEach((ele) => {
     items.push([ele.id, ele.BaseCommand, ele.UI_enabled, ele.NextTable]);
   });
 
-//   return (
-//     <div className='options'>
-//       <h1 id = 'heading'>Select Commands:</h1>
-//       <ButtonList items={items} />
-//       {showTextbox && (
-//         <div>
-//           <input
-//             type="text"
-//             value={textboxValue}
-//             onChange={handleTextboxChange}
-//             placeholder="Enter your input"
-//             className="textbox"
-//           />
-//           <button onClick={handleTextboxSubmit}>Submit</button>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Options;
-return (
-  <div className='options'>
-    <h1 id='heading'>Select Commands</h1>
-    <ButtonList items={items} />
-    {showTextbox && (
-      <div>
-        <input
-          type='text'
-          value={textboxValue}
-          onChange={handleTextboxChange}
-          placeholder='Enter your input'
-          className='textbox'
-        />
-        <button onClick={handleTextboxSubmit} className='button'>Submit</button>
-      </div>
-    )}
-  </div>
-);
+  return (
+    <div className='options'>
+      <h1 id = "heading">Select Commands</h1>
+      <ButtonList items={items} />
+      {showTextbox && (
+        <div>
+          <input
+            type="text"
+            value={textboxValue}
+            onChange={handleTextboxChange}
+            placeholder={showTextboxPlaceholder}
+            className = "textbox"
+          />
+          <button className = "button" onClick={handleTextboxSubmit}>Submit</button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Options;
