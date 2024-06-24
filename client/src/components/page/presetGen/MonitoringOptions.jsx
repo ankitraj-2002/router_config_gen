69f6fe106907e0cc92a3e74acf309d205c50b44d
Mayank Lohani <mayanklohani3@gmail.com>
2024-06-24 11:51:50 +0530

    updated preset for monitoring Options

--- client/src/components/page/presetGen/MonitoringOptions.jsx
+++ client/src/components/page/presetGen/MonitoringOptions.jsx
@@ -19,10 +19,21 @@ const CommandGenerator = () => {
     }
   };
 
-//   const handleGenerateBgpSummary = () => {
-//     setGeneratedCommand('show bgp summary');
-//   };
-
+  const handleGenerateBgpSummary = () => {
+    if(bgpSummary){
+      const code = `show bgp ${bgpSummary}`;
+      setGeneratedCommand(previous =>[...previous,code]);
+    }
+  };
+    const handleCopyAll= () => {
+    navigator.clipboard.writeText(generatedCommand.join('\n'))
+      .then(() => {
+        alert('Command copied to clipboard!');
+      })
+      .catch((err) => {
+        console.error('Failed to copy!', err);
+      });
+  };
   return (
     <div style={{ padding: '20px' }}>
       <h1>Command Generator</h1>
@@ -39,19 +50,42 @@ const CommandGenerator = () => {
           />
         </label>
         <button onClick={handleGenerateShowInt} style={{ marginRight: '10px' }}>
-          Generate 'show int'
+          For Interface info
         </button>
         <button onClick={handleGenerateMonitorInterface} style={{ marginRight: '10px' }}>
-          Generate 'monitor interface'
+          For Monitoring Interface
         </button>
-        {/* <button onClick={handleGenerateBgpSummary}>
-          Generate 'show bgp summary'
-        </button> */}
       </div>
-
-      {generatedCommand && (
+      <div style={{ marginBottom: '10px' }}>
+        <label>
+          show bgp:
+          <select
+            value={bgpSummary}
+            onChange={(e) => setBgpSummary(e.target.value)}
+            style={{ marginLeft: '10px' }}
+            required
+          >
+            <option value="">--Select Bgp Monitoring Options--</option>
+            <option value="ethernet-switching">summary</option>
+            <option value="summary group">summary group</option>
+            <option value="summary instance">summary instance</option>
+            <option value="summary extensive">summary extensive</option>
+            <option value="summary terse">summary terse</option>
+          </select>
+        </label>
+        <button onClick={handleGenerateBgpSummary} style={{ marginLeft: '20px' }}>
+          For Bgp Monitoring
+        </button>
+      </div>
+      {generatedCommand.length>0 && (
         <div style={{ marginTop: '20px' }}>
-          <h2>Generated Command:</h2>
+          <h2 style ={{display: 'inline-block'}}>Generated Code:</h2>
+          <button 
+            onClick={handleCopyAll} 
+            style={{ float: 'right', marginTop: '35px' }}
+          >
+            Copy All
+          </button>
           <pre>{generatedCommand.join('\n')}</pre>
         </div>
       )}
