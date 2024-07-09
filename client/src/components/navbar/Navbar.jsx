90cf3edf28086c11b5e37743774372073a8abacb
Ankit <ankitraj251202@gmail.com>
2024-07-09 13:03:40 +0530

    Navbar Completed

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 import './navbar.css';
 import stpilogo from "../images/logo.png";
 import logo from "../images/trial2.png";
+import homeActive from "../images/home-icon-active.png";
+import homeInactive from "../images/home-icon-inactive.png";
+import presetGenActive from "../images/presetGen-active.png";
+import presetGenInactive from "../images/presetGen-inactive.png";
+import configGenActive from "../images/configGen-active.png";
+import configGenInactive from "../images/configGen-inactive.png";
+import sshInactive from "../images/ssh-inactive.png";
+import sshActive from "../images/ssh-active.png";
+
 import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
@@ -10,10 +19,37 @@ export default function Navbar() {
     <div className='nav'>
       <img src={stpilogo} alt='logo' className='logo' />
       <ul>
-        <li><NavLink to ="/">Home</NavLink></li>
-        <li><NavLink to="/presetGen" >PresetGen</NavLink></li>
-        <li><NavLink to="/homepage" >ConfigGen</NavLink></li>
-        <li><NavLink to="/terminal" >Terminal</NavLink></li>
+        <li> <NavLink to="/" className='nav-link'>
+              {({ isActive }) => (
+                <>
+                  <img src={isActive ? homeActive : homeInactive} alt="" className='nav-icon' />
+                </>
+              )}
+            </NavLink></li>
+        <li><NavLink to="/presetGen"   className='nav-link'>{({ isActive }) => (
+                <>
+                  <img src={isActive ? presetGenActive :presetGenInactive} alt="" className='nav-icon' />
+                  <span>&nbsp;configGen</span>
+                </>
+              )}
+        </NavLink></li>
+        <li><NavLink to="/homepage"  className='nav-link'>
+        {({ isActive }) => (
+                <>
+                  <img src={isActive ? configGenActive :configGenInactive} alt="" className='nav-icon' />
+                  <span>&nbsp;configGen</span>
+                  
+                </>
+              )}</NavLink></li>
+        <li><NavLink to="/terminal" className='nav-link'>
+          {({ isActive }) => (
+                <>
+                  <img src={isActive ? sshActive :sshInactive} alt="" className='nav-icon' />
+                  <span>&nbsp;Terminal</span>
+                  
+                </>
+              )}
+        </NavLink></li>
       </ul>
     </div>
     <div className='logocontainer'>
@@ -22,3 +58,5 @@ export default function Navbar() {
     </div>
   );
 }
+
+
